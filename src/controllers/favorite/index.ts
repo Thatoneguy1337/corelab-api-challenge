ee7abebf59bd4918ad8bc8902b167fe50ff942fa
Victor Guterres <Vgb1337@gmail.com>
2023-10-02 01:15:29 -0300

    tarefa x finalizada

--- src/controllers/favorite/index.ts
+++ src/controllers/favorite/index.ts
@@ -1,5 +1,6 @@
 import { createFavoriteController } from "./createFavorite.controllers";
 import { listAllFavoriteController } from "./retrieveFavorite.controllers";
 import { deleteFavoriteController } from "./deleteFavorite.controllers";
+import { updateFavoriteController } from "./updateFavorite.controllers";
 
-export {createFavoriteController, listAllFavoriteController, deleteFavoriteController};
+export {createFavoriteController, listAllFavoriteController, deleteFavoriteController,updateFavoriteController};
